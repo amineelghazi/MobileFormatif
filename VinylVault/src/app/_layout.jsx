974c7505387f406bef140fa32878b570b848794f
amineelghazi <amineelghazi100@hotmail.com>
2026-09-20 21:26:38 -0400

    project setup

---
+++ VinylVault/src/app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RootLayout = () => {
+  return (
+    <View style={{flex: 1,justifyContent:'center'}}>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
